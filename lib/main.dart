389a96eaf57aac5bfee27de602b6cc5c9da7766f
Swapnika Agrawal <swapnikaagrawal@gmail.com>
2022-09-05 16:24:29 +0530

    my_app1 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MaterialApp(
+    title: "Bello!!",
+    home:myApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 76, 127, 175),
-          title: const Center(
-              child: Text(
-            "Hello!!",
-          )),
-        ),
-        drawer: Drawer(
-          child: Container(
-            child: ListView(
-              children: [
-                const DrawerHeader(
-                  
-                  child: ListTile(
-                    title: Text(
-                      'Hello I am a drawer header',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  decoration:
-                      BoxDecoration(color: Color.fromARGB(255, 76, 127, 175)),
-                      
-                )
-              ],
-            ),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bello!!"),
+      ),
+      body: Container(
+        child: Center(child: Text("This is my first flutter app")),
       ),
     );
   }
