c91779cbf476087d5bf03e17ca8b3c950971b97c
Swapnika Agrawal <swapnikaagrawal@gmail.com>
2022-09-05 20:23:26 +0530

    my_app2 done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: "Bello!!",
     home:myApp(),
   ));
 }
@@ -14,10 +13,23 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bello!!"),
+        title: Text("Bello!!", style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
       ),
-      body: Container(
-        child: Center(child: Text("This is my first flutter app")),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: Text("Watch Me", style: TextStyle(color: Colors.white),),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [Colors.pink, Colors.yellow,]),
+            boxShadow: [
+              BoxShadow(color: Colors.grey, blurRadius: 10, spreadRadius: 1, offset: Offset(4, 5)),
+            ]
+          ),
+        ),
       ),
     );
   }
